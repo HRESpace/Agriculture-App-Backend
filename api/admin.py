3db6e285bcc6769facf36be0cce3dee2e5a61296
HRESpace <harsh.raj@erishaspace.com>
2023-04-14 17:55:30 +0530

    Seventh Commit

--- api/admin.py
+++ api/admin.py
@@ -6,6 +6,5 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user_name', 'date','time','imgUri','crop_type','crop_season','crop_stage','plot_area',
-                    'village','district_or_mandal_name','state','field_owner','land_type','soil_type',
-                    'soil_moisture','prev_crop','approx_yield','yield_history']
+    list_display = ['id', 'user_name', 'email_id', 'date','imgUri','crop_type','crop_season', 'village_district',
+                    'state','field_owner','soil_type','prev_crop','yield_history']
